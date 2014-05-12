4a8c35f1ba6e81fe1a067ee45679ec3088749d90
diki <819348195@qq.com>
2014-05-12 11:20:52 +0800

    提交

---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\testplugin\platforms\android\bin\classes.dex : \
+C:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120732\testplugin\BuildConfig.class \
+C:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120732\testplugin\R$attr.class \
+C:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120732\testplugin\R$drawable.class \
+C:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120732\testplugin\R$string.class \
+C:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120732\testplugin\R$xml.class \
+C:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120732\testplugin\R.class \
+C:\testplugin\platforms\android\bin\classes\cn\edu\gdmec\x07120732\testplugin\testplugin.class \
+C:\testplugin\platforms\android\bin\dexedLibs\classes-e52c6e8428f22ec426fbbf812b7246f1.jar \
